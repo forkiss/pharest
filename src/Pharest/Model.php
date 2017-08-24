3faa3397ca19736bef1351b0b65dd8536b605fc7
Clover <forkissed@gmail.com>
2017-08-24 23:21:19 +0800

    notNullValidations

--- src/Pharest/Model.php
+++ src/Pharest/Model.php
@@ -8,6 +8,15 @@ class Model extends \Phalcon\Mvc\Model
 
     protected static $datetime;
 
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        \Phalcon\Mvc\Model::setup(['notNullValidations' => false]);
+        $this->setSchema("ebao");
+    }
+
     public static function setDatetime($datetime)
     {
         self::$datetime = $datetime;
@@ -54,9 +63,9 @@ class Model extends \Phalcon\Mvc\Model
         $query = parent::findFirst($parameters);
 
         if (!$query) {
-            
+
             if (!$message) {
-                $message = self::getSchema() . ' data can not be found';
+                $message = 'data can not be found';
             }
 
             throw new \Pharest\Exception\ModelException($message, 100091);
@@ -65,7 +74,7 @@ class Model extends \Phalcon\Mvc\Model
         return $query;
     }
 
-    public function store($data = NULL)
+    public function store($data = null)
     {
         if (parent::save($data) === false) {
             throw new \Pharest\Exception\ModelException(implode(',', $this->getMessages()), 100090);
@@ -74,14 +83,14 @@ class Model extends \Phalcon\Mvc\Model
 
     public function create($data = null, $whiteList = null)
     {
-        if (parent::create($data) === false) {
+        if (parent::create($data, $whiteList) === false) {
             throw new \Pharest\Exception\ModelException(implode(',', $this->getMessages()), 100090);
         }
     }
 
     public function update($data = null, $whiteList = null)
     {
-        if (parent::update($data) === false) {
+        if (parent::update($data, $whiteList) === false) {
             throw new \Pharest\Exception\ModelException(implode(',', $this->getMessages()), 100090);
         }
     }
